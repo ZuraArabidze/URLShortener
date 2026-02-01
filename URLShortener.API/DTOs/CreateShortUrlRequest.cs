96d18f4315ed7848770e103567cf2696b61aeb6c
ZuraArabidze <z.arabidzee@gmail.com>
2026-02-02 00:44:41 +0400

    add DTOs for request/response

---
+++ URLShortener.API/DTOs/CreateShortUrlRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace URLShortener.API.DTOs;
+
+public class CreateShortUrlRequest
+{
+    public string OriginalUrl { get; set; } = string.Empty;
+    public string? CustomShortCode { get; set; }
+    public DateTime? ExpiresAt { get; set; }
+}
---
+++ URLShortener.API/DTOs/ErrorResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace URLShortener.API.DTOs;
+
+public class ErrorResponse
+{
+    public string Message { get; set; } = string.Empty;
+    public string? Details { get; set; }
+    public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+}
