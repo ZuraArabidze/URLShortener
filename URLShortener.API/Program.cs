e04aa2dabe0a425ec414cb12a8543f46f3e844ff
ZuraArabidze <z.arabidzee@gmail.com>
2026-02-05 00:08:05 +0400

    configure application startup

--- URLShortener.API/Program.cs
+++ URLShortener.API/Program.cs
@@ -1,6 +1,88 @@
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+using StackExchange.Redis;
+using URLShortener.API.Middleware;
+using URLShortener.Core.Interfaces;
+using URLShortener.Core.Services;
+using URLShortener.Infrastructure.Data;
+using URLShortener.Infrastructure.Repositories;
+using URLShortener.Infrastructure.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Configure Serilog
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .CreateLogger();
+
+builder.Host.UseSerilog();
+
+// Add services to the container
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Configure PostgreSQL with Entity Framework
+builder.Services.AddDbContext<UrlShortenerDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Configure Redis
+var redisConnectionString = builder.Configuration.GetConnectionString("Redis")
+    ?? throw new InvalidOperationException("Redis connection string not found.");
+
+builder.Services.AddSingleton<IConnectionMultiplexer>(sp =>
+{
+    var configuration = ConfigurationOptions.Parse(redisConnectionString);
+    configuration.AbortOnConnectFail = false;
+    return ConnectionMultiplexer.Connect(configuration);
+});
+
+// Register services
+builder.Services.AddScoped<IUrlRepository, UrlRepository>();
+builder.Services.AddScoped<ICacheService, RedisCacheService>();
+builder.Services.AddScoped<IUrlService, UrlService>();
+
+// Add CORS
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Configure the HTTP request pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
-app.Run();
+// Custom middleware
+app.UseMiddleware<RequestLoggingMiddleware>();
+app.UseMiddleware<ExceptionHandlingMiddleware>();
+
+app.UseHttpsRedirection();
+app.UseCors();
+app.UseAuthorization();
+app.MapControllers();
+
+Log.Information("Starting URL Shortener API...");
+
+try
+{
+    app.Run();
+    Log.Information("Application stopped gracefully.");
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
