045345e04454fc88edfb40dda57b854518139b8f
ZuraArabidze <z.arabidzee@gmail.com>
2026-02-05 00:45:37 +0400

    add migrations, change dotnet version to 8

---
+++ URLShortener.Infrastructure/Migrations/20260204203407_InitialCreate.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace URLShortener.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ShortenedUrls",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    OriginalUrl = table.Column<string>(type: "character varying(2048)", maxLength: 2048, nullable: false),
+                    ShortCode = table.Column<string>(type: "character varying(10)", maxLength: 10, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ExpiresAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    ClickCount = table.Column<int>(type: "integer", nullable: false, defaultValue: 0),
+                    CreatedByIp = table.Column<string>(type: "character varying(45)", maxLength: 45, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShortenedUrls", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShortenedUrls_ShortCode",
+                table: "ShortenedUrls",
+                column: "ShortCode",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ShortenedUrls");
+        }
+    }
+}
